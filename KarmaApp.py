13210680fd7d3ab09a5c187bb212fb0d9eaee24d
Antonio Lignan <alinan@zolertia.com>
2014-09-04 11:06:01 +0200

    Changed message format

--- KarmaApp.py
+++ KarmaApp.py
@@ -17,10 +17,15 @@ import time
 import os
 import pyGalileo as pyGal
 
+MY_ID = "ABCD"
+
 wait_until_subscribed = 1
 
 ETHERNET_ADDR = "169.254.1.232"
 ETHERNET_MASK = "255.255.0.0"
+
+# Fixed Location, latitude, longitude
+LOCATION = "43.94305555555555, 15.45346111111111"
 
 # When connected, subscribe to out topics of interest
 def on_connect(mosq, obj, rc):
@@ -45,6 +50,15 @@ def on_subscribe(mosq, obj, mid, granted_qos):
 def on_log(mosq, obj, level, string):
     print(string)
 
+def create_json_message(msg_type, msg_val, msg_string):
+    string = "{alarm_type:"
+    string = string + msg_type + "; alarm_value:"
+    string = string + msg_val + "; alarm_msg:"
+    string = string + msg_string + "; location:"
+    string = string + location + "; id:"
+    string = string + MY_ID
+    return string
+
 # Create an ethernet backdoor if directly connected to the board
 os.system("telnetd -l /bin/sh");
 os.system("ifconfig eth0 {0} netmask {1} up".format(ETHERNET_ADDR, ETHERNET_MASK));
@@ -63,6 +77,10 @@ TOPIC_ALARM_SEVERE = "/senzations/citizenkarmakit/panic_alert"
 TOPIC_ALARM_ASSIST = "/senzations/citizenkarmakit/assistant_alert"
 TOPIC_MIC_ALARM    = "/senzations/citizenkarmakit/mic_alert"
 
+MSG_ALARM_SEVERE = "Someone needs urgent help at"
+MSG_ALARM_ASSIST = "Someone could use some help at"
+MSG_ALARM_SHOUT  = "A shout has been detected at"
+
 ID_ALARM_SEVERE = 3
 ID_ALARM_MIC    = 2
 ID_ALARM_ASSIST = 1
@@ -73,12 +91,15 @@ buttonPin_assistant = 3
 sensor_mic = "A0"
 SENSOR_MIC_THR = 150
 
+MQTT_SERVER = "iot.eclipse.org"
+MQTT_PORT = 1883
+
 # configure digital port for button readout
 pyGal.pinMode(buttonPin_severe, pyGal.INPUT)
 pyGal.pinMode(buttonPin_assistant, pyGal.INPUT)
 
 # connect to a mosquitto test broker on the web
-mqttc.connect("test.mosquitto.org", 1883, 60)
+mqttc.connect(MQTT_SERVER, MQTT_PORT, 60)
 
 # And create an alarm flag, the alarms should be placed from lower priority to higher,
 # so it will overwrite any low-priority alarm/event
@@ -92,47 +113,39 @@ while True:
     pressed_severe = pyGal.digitalRead(buttonPin_severe)
     pressed_assist = pyGal.digitalRead(buttonPin_assistant)
 
-    if (wait_until_subscribed) return
+    if not wait_until_subscribed:
 
-    data_string = str(ID_ALARM_ASSIST) + ":"
-    if (pressed_assist == pyGal.HIGH):  
-      alarm_flag = TOPIC_ALARM_ASSIST
-      data_string = data_string + "HIGH"
-    else:
-      data_string = data_string + "LOW"
-    
-    # Check the status of the analog sensors
-    sensor_mic_val = pyGal.analogRead(sensor_mic)
+        # Checks the assist button status
+        if (pressed_assist == pyGal.HIGH):
+            alarm_flag = TOPIC_ALARM_ASSIST
+            data_string = create_json_message(ID_ALARM_ASSIST, "HIGH", MSG_ALARM_ASSIST)
+        
+        # Check the status of the analog sensors
+        sensor_mic_val = pyGal.analogRead(sensor_mic)
 
-    data_string = data_string + " " + str(ID_ALARM_MIC) + ":" + str(sensor_mic_val) + ":"
-    if (sensor_mic_val >= SENSOR_MIC_THR):
-        alarm_flag = TOPIC_MIC_ALARM
-        data_string = data_string + "HIGH"
-    else:
-        data_string = data_string + "LOW"
+        if (sensor_mic_val >= SENSOR_MIC_THR):
+            alarm_flag = TOPIC_MIC_ALARM
+            data_string = create_json_message(ID_ALARM_MIC, str(sensor_mic_val), MSG_ALARM_SHOUT)
 
-    # Check if any button was pressed
-    data_string = data_string + " " + str(ID_ALARM_SEVERE) + ":"
-    if (pressed_severe == pyGal.HIGH):
-        alarm_flag = TOPIC_ALARM_SEVERE
-        data_string = data_string + "HIGH"
-    else:
-        data_string = data_string + "LOW"
+        # Check if severe alarm button has been pressed
+        if (pressed_severe == pyGal.HIGH):
+            alarm_flag = TOPIC_ALARM_SEVERE
+            data_string = create_json_message(ID_ALARM_SEVERE, "HIGH", MSG_ALARM_SEVERE)
 
-    if (alarm_flag):
-        print "Sending a message to the broker in 1 second:"
-        print alarm_flag, data_string
+        if (alarm_flag):
+            print "Sending a message to the broker in 1 second:"
+            print data_string
 
-        time.sleep(1)
+            time.sleep(1)
 
-        # Check to which topic should I post, the priority is post to critital alarms first!
-        mqttc.publish(alarm_flag, data_string)
+            # Check to which topic should I post, the priority is post to critital alarms first!
+            # mqttc.publish(alarm_flag, data_string)
 
-        # Clear the alarm afterwards
-        alarm_flag = ""
+            # Clear the alarm afterwards
+            alarm_flag = ""
 
-    #  and data (we not collect data)
-    data_string = ""
+        #  and data (we not collect data)
+        data_string = ""
 
     # and backoff a bit
     time.sleep(0.1)
